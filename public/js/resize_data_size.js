9fe2a9e2314c105b16726f1642d42c15a6b3b9ee
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-11-03 14:04:33 +0900

    全要素対応
    日ごと以外の「週、月、年」の要素にも対応

--- public/js/resize_data_size.js
+++ public/js/resize_data_size.js
@@ -1,21 +1,50 @@
 function resizeDataSize(){
-    var check_day = document.getElementById("day_div").firstElementChild;
-    var elem_day = document.getElementById("day_div").children;
-    var elem_lg = document.getElementById("day_div").childElementCount
+    /* for関数で回す用のkeyとバリューの設定
+     * key: getElementByIdで取得するid
+     * value: keyの子要素数
+    */
+    var elem_list = {
+        "day_div": document.getElementById("day_div").childElementCount,
+        "week_div": document.getElementById("week_div").childElementCount,
+        "month_div": document.getElementById("month_div").childElementCount,
+        "year_div": document.getElementById("year_div").childElementCount
+    };
 
     if(window.innerWidth < 960){
-        if(getComputedStyle(check_day).height == "54px"){
-            for(var i = 0; i < elem_lg; i++){
-                elem_day[i].style.height = "162px"
-                elem_day[i].style.opacity = "1"
-            }   
+        for(i in elem_list){
+            var elem_first = document.getElementById(i).firstElementChild;
+            var elem = document.getElementById(i).children;
+            if(getComputedStyle(elem_first).height == "54px"){
+                for(var j = 0; j < elem_list[i]; j++){
+                    elem[j].style.height = "162px"
+                    elem[j].style.opacity = "1"
+                }
+            // 開かれていないデータはブラウザをリサイズしても変更しない
+            } else if(getComputedStyle(elem_first).height == "0px"){
+                for(var j = 0; j < elem_list[i]; j++){
+                    elem[j].style.height = "0px"
+                    elem[j].style.opacity = "0"
+                }
+            }
+            
         }
     } else {
-        if(getComputedStyle(check_day).height == "162px"){
-            for(var i = 0; i < elem_lg; i++){
-                elem_day[i].style.height = "54px"
-                elem_day[i].style.opacity = "1"
+        for(i in elem_list){
+            var elem_first = document.getElementById(i).firstElementChild;
+            var elem = document.getElementById(i).children;
+            if(getComputedStyle(elem_first).height == "162px"){
+                for(var j = 0; j < elem_list[i]; j++){
+                    elem[j].style.height = "54px"
+                    elem[j].style.opacity = "1"
+                }
+            // 開かれていないデータはブラウザをリサイズしても変更しない
+            } else if(getComputedStyle(elem_first).height == "0px"){
+                for(var j = 0; j < elem_list[i]; j++){
+                    elem[j].style.height = "0px"
+                    elem[j].style.opacity = "0"
+                }
             }
+            
         }
     }
 }
