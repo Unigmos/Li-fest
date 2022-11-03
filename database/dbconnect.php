d591bceaa482ad5a3fab9caaf93610f323a77f4b
ryo_DPC <treehouse12101@gmail.com>
2022-11-03 13:13:47 +0900

    データベース接続

---
+++ database/dbconnect.php
@@ -0,0 +1,26 @@
+<?php
+require_once "./env.php";
+
+function connect(){
+    $host = DB_HOST;
+    $dbname = DB_NAME;
+    $user = DB_USER;
+    $pass = DB_PASS;
+
+    /*データベース情報 (Data Source Name)*/ 
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    /*接続オプション*/
+    $driver_options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ];
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $driver_options);
+    } catch (PDOException $e) {
+        echo "failed connect".$e->getMessage();
+        exit();
+    }
+
+}
