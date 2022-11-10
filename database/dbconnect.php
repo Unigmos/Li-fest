b7d54d6a4b134b6432130788ec90baa7945cf564
ryo_DPC <treehouse12101@gmail.com>
2022-11-11 05:27:27 +0900

    ItemDBConnect追加、元の関数名変更

--- database/dbconnect.php
+++ database/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 require_once "./env.php";
 
-function connect(){
+function accountDbConnect(){
     $host = DB_HOST;
-    $dbname = DB_NAME;
+    $dbname = DB_NAME_ACCOUNT;
     $user = DB_USER;
     $pass = DB_PASS;
 
@@ -23,5 +23,28 @@ function connect(){
         echo "failed connect".$e->getMessage();
         exit();
     }
+}
 
+function itemDbConnect(){
+    $host = DB_HOST;
+    $dbname = DB_NAME_ITEM;
+    $user = DB_USER;
+    $pass = DB_PASS;
+
+    /*データベース情報 (Data Source Name)*/ 
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    /*接続オプション*/
+    $driver_options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ];
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $driver_options);
+        echo "ok2";
+    } catch (PDOException $e) {
+        echo "failed connect".$e->getMessage();
+        exit();
+    }
 }
