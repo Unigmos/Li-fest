399da70bbeaadd0b47ebb5ee77af00fe2cf46f23
ryo_DPC <treehouse12101@gmail.com>
2022-11-15 23:45:57 +0900

    追加

--- database/env.php
+++ database/env.php
@@ -1,7 +1,7 @@
 <?php
 
 define("DB_HOST", "1");
-define("DB_USER", "22");
-define("DB_NAME_ACCOUNT","333");
-define("DB_NAME_ITEM", "4444");
+define("DB_NAME_ACCOUNT","22");
+define("DB_NAME_ITEM", "333");
+define("DB_USER", "4444");
 define("DB_PASS", "55555");
