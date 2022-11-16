65cb17d6412ad3428c2ca899bedef3b0c7397545
ryo_DPC <treehouse12101@gmail.com>
2022-11-16 14:55:50 +0900

    インプットボックスに入力されているかチェック

--- public/js/checkinput.js
+++ public/js/checkinput.js
@@ -1,12 +1,34 @@
-function checkInput(){
-    var phone_num = document.getElementsByName(phone_num).values;
-    var password = document.getElementsByName(password).values;
-    var login_button = document.getElementsByName(login_button);
-    if(phone_num!="" && password!=""){
-        login_button.removeAttribute("disable");
-    }else{
-        login_button.setAttribute("disable", true);
+var button;
+console.log(document.forms);
+var formlength = document.forms[0].length;
+// TODO:account_registerのほうでlengthがなぜかエラー
+var inputs = new Array(formlength-1);
+for (let i = 0; i < formlength; i++) {
+    let input = document.forms[0].elements[i];
+    if(input.type=="text" || input.type=="password"){
+        inputs[i] = document.forms[0].elements[i];
+        console.log(inputs[i]);
+    }else if(input.type=="submit"){
+        button = input;
     }
 }
 
-// TODO:未完成あとでーーーーーーやる引数いれｔ
+
+function buttonActivation(){
+    if (inputs.every(checkInput)!="") {
+        button.removeAttribute("disabled");
+    }else{
+        if (!button.hasAttribute("disabled")) {
+            button.setAttribute("disabled","disabled");
+        }
+    }
+}
+
+function checkInput(input){
+    return input.value!="";
+}
+
+
+
+
+
