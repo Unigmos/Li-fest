d815cafeba68e2eaf9686d44410de70b8b5f5af6
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-11-19 14:10:27 +0900

    画面サイズが大きくなった時にも対応

--- public/js/replace.js
+++ public/js/replace.js
@@ -3,25 +3,32 @@ function Replace(){
     const data_count = document.querySelector(".how_to_use_content").childElementCount;
     const datas = document.querySelector(".how_to_use_content").children;
     const check_class = "replace_data";
+    const image_class = "use_image";
+    const sentence_class = "use_sentence";
     if(window.innerWidth < browser_size){
         for(var i = 0; i < data_count; i++){
             // 子要素のクラス名を取得する
             var data = datas[i].children;
             // 「check_class」が含まれるデータに関しては、順番を入れ替える。
             if(datas[i].classList.contains(check_class)){
-                console.log("True")
                 // 画像→文章の並びの時のみ入れ替え
-                if(data[0].className == "use_image" && data[1].className == "use_sentence"){
+                if(data[0].className == image_class && data[1].className == sentence_class){
                     datas[i].insertBefore(data[1], data[0])
                 }
-            } else {
-                console.log("False")
             }
         }
     } else {
-        ;
-        // TODO:元に戻す部分の追加
-        // TODO:load時のも作る
+        for(var i = 0; i < data_count; i++){
+            // 子要素のクラス名を取得する
+            var data = datas[i].children;
+            // 「check_class」が含まれるデータに関しては、順番を入れ替える。
+            if(datas[i].classList.contains(check_class)){
+                // 文章→画像の並びの時のみ入れ替え
+                if(data[0].className == sentence_class && data[1].className == image_class){
+                    datas[i].insertBefore(data[1], data[0])
+                }
+            }
+        }
     }
 }
 
