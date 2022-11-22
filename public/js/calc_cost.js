f61d3522c02e71f49d9cdf077d7414e0b98b12dd
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-11-22 20:51:17 +0900

    log削除&変数説明追加

--- public/js/calc_cost.js
+++ public/js/calc_cost.js
@@ -10,14 +10,22 @@ function CalcCost(){
         "month": document.getElementsByClassName("month_quantity").length,
         "year": document.getElementsByClassName("year_quantity").length
     };
-    // 各要素(日、週、月、年)ごとの合計の連想配列
+    /* 
+     * 各要素(日、週、月、年)ごとの合計の連想配列
+     * key: このJSファイル内のみで参照する値(クラス名やid名ではない)
+     * value: それぞれの合計金額円(日、週、月、年)
+    */
     var resp_cost = {
         "day_resp": 0,
         "week_resp": 0,
         "month_resp": 0,
         "year_resp": 0
     };
-    // 文章データの連想配列
+    /* 
+     * 文章データの連想配列
+     * key: 変数「calc_list」と同じ値→for関数で回すときに連想配列は異なっても同じ名前のkeyとvalueを取得するため
+     * value: 書き換えに用いるそれぞれの要素(日、週、月、年)の文字列データ
+    */
     var sentence_list = {
         "day": "日",
         "week": "週",
@@ -28,6 +36,7 @@ function CalcCost(){
     // 要素ごとの合計料金を求める
     for(i in calc_list){
         for(var j = 0; j < calc_list[i]; j++){
+            // TODO:(数値のみ入力だが、仮に文字列が入った時用の例外処理)
             var count = parseInt(document.getElementsByClassName(i + "_quantity")[j].textContent);
             var cost = parseInt(document.getElementsByClassName(i + "_cost")[j].textContent);
             // 直前の値読み取り、追加して上書き
@@ -40,7 +49,6 @@ function CalcCost(){
         var write_elem = document.getElementById(access_id);
         write_elem.innerHTML = `${sentence_list[k]}ごと合計:${resp_cost[k + "_resp"]}円`;
     }
-    console.log(resp_cost)
 }
 
 window.addEventListener('load', CalcCost);
