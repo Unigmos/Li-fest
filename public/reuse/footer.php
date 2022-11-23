e52cb7786a1fcc3886c6c08bac0cac65636c7623
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-11-23 15:25:54 +0900

    php化

---
+++ public/reuse/footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<footer>
+    <div class="footer_container">
+        <div class="centering">
+            <img src="/public/image/Li-fest_transparent.png" alt="logo">
+        </div>
+        <!-- TODO:リンク先指定 -->
+        <ul>
+            <li><a href="#">利用規約</a></li>
+            <li><a href="#">開発者情報</a></li>
+            <li><a href="#">プライバシーポリシー</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+        </ul>
+        <div class="centering">
+            <p>Copyright © 2022-<?php echo date('Y') ?> Li-fest All Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
