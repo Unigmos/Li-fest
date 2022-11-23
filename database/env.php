c8db91e257ebabea527072bb7638a6be97a4a0b4
ryo_DPC <treehouse12101@gmail.com>
2022-11-23 16:25:23 +0900

    なにもない

--- database/env.php
+++ database/env.php
@@ -1,6 +1,6 @@
 <?php
 
 define("DB_HOST", "1");
-define("DB_NAME","22");
-define("DB_USER", "333");
-define("DB_PASS", "4444");
+define("DB_NAME", "2");
+define("DB_USER", "3");
+define("DB_PASS", "4");
