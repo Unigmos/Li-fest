9571ef87116828ce410fdd81484d506b31304cae
ryo_DPC <treehouse12101@gmail.com>
2022-11-23 15:41:25 +0900

    connectに戻した

--- database/dbconnect.php
+++ database/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
-require_once "./env.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/database/env.php";
 
-function accountDbConnect(){
+function connect(){
     $host = DB_HOST;
-    $dbname = DB_NAME_ACCOUNT;
+    $dbname = DB_NAME;
     $user = DB_USER;
     $pass = DB_PASS;
 
@@ -18,32 +18,6 @@ function accountDbConnect(){
 
     try {
         $pdo = new PDO($dsn, $user, $pass, $driver_options);
-        echo "ok";
-        return $pdo;
-    } catch (PDOException $e) {
-        echo "failed connect".$e->getMessage();
-        exit();
-    }
-}
-
-function itemDbConnect(){
-    $host = DB_HOST;
-    $dbname = DB_NAME_ITEM;
-    $user = DB_USER;
-    $pass = DB_PASS;
-
-    /*データベース情報 (Data Source Name)*/ 
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-    /*接続オプション*/
-    $driver_options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ];
-
-    try {
-        $pdo = new PDO($dsn, $user, $pass, $driver_options);
-        echo "ok2";
         return $pdo;
     } catch (PDOException $e) {
         echo "failed connect".$e->getMessage();
