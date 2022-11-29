b85a3f6e2d921dc5f25872fe14d63e35e62b71f8
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-11-29 15:52:08 +0900

    開発者用ファイル

---
+++ public/dev_info.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <!-- head -->
+    <?php include($_SERVER["DOCUMENT_ROOT"]."/public/reuse/head.html");?>
+    <link rel="stylesheet" href="css/dev_info.css">
+    <link rel="stylesheet" href="css/reuse/header.css">
+    <link rel="stylesheet" href="css/reuse/footer.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- header -->
+    <?php include($_SERVER["DOCUMENT_ROOT"]."/public/reuse/header.html");?>
+    <main class="main_container">
+        <div>
+
+        </div>
+    </main>
+    <!-- footer -->
+    <?php include($_SERVER["DOCUMENT_ROOT"]."/public/reuse/footer.php");?>
+</body>
+</html>
