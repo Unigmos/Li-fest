d941aad4f2680a2850fc2ccca4625d993534701e
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-11-29 17:10:21 +0900

    コンテンツ量が少ないときにフッターを画面下部に固定するプログラム

---
+++ public/js/under_fix.js
@@ -0,0 +1,23 @@
+function UnderFix(){
+    // 各種高さの取得
+    var container_height = document.querySelector(".main_container").clientHeight;
+    var window_height = window.innerHeight;
+    var footer_height = document.querySelector(".footer_container").clientHeight;
+    var header_height = document.querySelector(".header_container").clientHeight;
+    // スタイル指定用の要素
+    var footer_elem = document.querySelector(".footer_container");
+
+    // 画面の高さからヘッダーとフッターの高さを引く(これで比較を行う)
+    var check_height = window_height - header_height - footer_height;
+    // 要素の高さと画面の高さを比較し要素の高さの方が小さい場合は下部に固定する
+    if (container_height < check_height){
+        footer_elem.style.position = "fixed";
+        footer_elem.style.bottom = "0";
+    } else {
+        footer_elem.style.position = "static";
+        footer_elem.style.bottom = "auto";
+    }
+}
+
+window.addEventListener('load', UnderFix);
+window.addEventListener('resize', UnderFix);
