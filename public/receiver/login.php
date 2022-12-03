560adbb57db3ea3e46f23d23675dae1d631e09a7
ryo_DPC <treehouse12101@gmail.com>
2022-12-03 15:44:14 +0900

    ログインセッション作成

--- public/receiver/login.php
+++ public/receiver/login.php
@@ -7,19 +7,29 @@ $pass = filter_input(INPUT_POST, "password");
 // 基本ないけど、何か起きて値が入っていなかったらレダイレクトで戻る
 if (!isset($email) || !isset($pass)) {
     header("location: /public/loginform.php");
+    $pass=0;
+    exit();
 }
 $array = array("email"=>$email,"password"=>$pass);
 $hasCreated = Account::searchAcount($array);
 
 if ($hasCreated) {
     header("location: \public\confirmation.php");
+    session_start();
+    $_SESSION['user'] = $email;
+    $email=0;
+    $pass=0;
+    $array=[];
+    $_POST=[];
     exit();
 }elseif ($hasCreated == 0){
     // TODO:エラーメッセージ送信　パスワードが違う
     header("location: /public/loginform.php");
+    exit();
 }else{
     // TODO:エラーメッセージ送信　emailが違う
     header("location: /public/loginform.php");
+    exit();
 }
 
 ?>
