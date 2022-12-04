56f83999c21ea6b59653a550dc72e0a82b68fedd
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-12-04 16:49:21 +0900

    お問い合わせフォーム

---
+++ public/contact_form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <!-- head -->
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/public/reuse/head.html";?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/css/contact_form.css">
+    <script src="/public/js/under_fix.js" async></script>
+</head>
+<body>
+    <!-- header -->
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/public/reuse/header.html";?>
+    <main class="main_container">
+        <div class="contact_title">
+            <h2>お問い合わせ</h2>
+        </div>
+    </main>
+    <!-- footer -->
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/public/reuse/footer.php";?>
+</body>
+</html>
