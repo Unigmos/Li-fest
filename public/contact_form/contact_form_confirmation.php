ce1c92c63896961a98eb0c8aa47ee873e8448c99
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-12-06 17:17:53 +0900

    お問い合わせ確認画面

---
+++ public/contact_form/contact_form_confirmation.php
@@ -0,0 +1,38 @@
+<?php
+	if($_SERVER["REQUEST_METHOD"] != "POST" || !isset($_POST['csrf_token']) || $_POST['csrf_token'] != $_SESSION['csrf_token']){
+		header("Location: index.php");
+		exit();
+	}
+
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <!-- head -->
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/public/reuse/head.html";?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/css/contact_form.css">
+    <script src="/public/js/under_fix.js" async></script>
+</head>
+<body>
+    <!-- header -->
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/public/reuse/header.html";?>
+    <main class="main_container">
+        <div class="contact_title">
+            <h2>お問い合わせ確認</h2>
+            <form action="send.php" method="post">
+                <h3>メールアドレス</h3>
+                <?php echo htmlspecialchars($email, ENT_QUOTES,'UTF-8');?>
+                <h3>お問い合わせ内容</h3>
+                <p><?php echo htmlspecialchars($message, ENT_QUOTES,'UTF-8');?></p>
+            </form>
+        </div>
+    </main>
+    <!-- footer -->
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/public/reuse/footer.php";?>
+</body>
+</html>
