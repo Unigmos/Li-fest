a55a6cf500394dd771f8a63611e9954d7aa4e6b8
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-12-14 20:19:47 +0900

    ドキュメントルート設定

--- public/reuse/footer.php
+++ public/reuse/footer.php
@@ -2,14 +2,14 @@
 <footer>
     <div class="footer_container">
         <div class="centering">
-            <img src="/public/image/Li-fest_transparent.png" alt="logo">
+            <img src="/image/Li-fest_transparent.png" alt="logo">
         </div>
         <div class="footer_nav">
             <ul class="footer_nav_ul">
-                <li><a href="/public/terms_of_service.php">利用規約</a></li>
-                <li><a href="/public/dev_info.php">開発者情報</a></li>
-                <li><a href="/public/privacy_policy.php">プライバシーポリシー</a></li>
-                <li><a href="/public/contact_form/contact_form.php">お問い合わせ</a></li>
+                <li><a href="/terms_of_service.php">利用規約</a></li>
+                <li><a href="/dev_info.php">開発者情報</a></li>
+                <li><a href="/privacy_policy.php">プライバシーポリシー</a></li>
+                <li><a href="/contact_form/contact_form.php">お問い合わせ</a></li>
             </ul>
         </div>
         <div class="centering">
